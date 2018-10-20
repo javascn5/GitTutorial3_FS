916306b3077d9fae9e1948a95525d01a503c44c1
Meandester <filipsek@live.com>
2018-10-20 14:59:07 +0200

    zmiany w main

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git Tutorial 3");
 
+        System.out.println("dodałem kod na masterze");
+
 	// write your code here
     }
 }
